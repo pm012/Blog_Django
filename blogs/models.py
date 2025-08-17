46517570260070d70caaf6166f0deb22ea5d5999
Serhii Kroshka <pm012@ukr.net>
2025-08-18 00:07:02 +0300

    Added owner checks for blogs (blog records should be shown and can be edited strictly by owner)

--- blogs/models.py
+++ blogs/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class BlogPost(models.Model):  
     title = models.CharField(max_length=200)
     text = models.TextField()
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # Assuming 'auth.User' is the user model used in your project
+    # If you have a custom user model, replace 'auth.User' with your model's name
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
--- blogs/views.py
+++ blogs/views.py
@@ -1,23 +1,36 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import BlogPost
 from .forms import BlogPostForm
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
 
+def check_blog_ownership(request, post):
+    """Check if the logged-in user is the owner of the blog post."""
+    if post.owner != request.user:
+        raise Http404("You do not have permission to edit this post.")
+
+@login_required
 def index(request):
-    posts = BlogPost.objects.all()
+    posts = BlogPost.objects.filter(owner=request.user)  # Only show posts for the logged-in user
     return render(request, 'blogs/index.html', {'posts': posts})
 
 def new_post(request):
     if request.method == 'POST':
         form = BlogPostForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            post.owner = request.user  # Set the owner to the logged-in user
+            post.save()
+            #form.save()
             return redirect('blogs:index')
     else:
         form = BlogPostForm()
     return render(request, 'blogs/new_post.html', {'form': form})
 
+@login_required
 def edit_post(request, post_id):
     post = get_object_or_404(BlogPost, id=post_id)
+    check_blog_ownership(request, post)
     if request.method == 'POST':
         form = BlogPostForm(request.POST, instance=post)
         if form.is_valid():
